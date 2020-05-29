84937d3d509a0cc0bfed1cab98ff4acaf366ed4e
MarianoNavarrete <marianoskt14@hotmail.com>
2020-05-29 09:07:12 -0500

    agregando todos los archivos del proyecto

---
+++ Views/modules/usuarios/salir.php
@@ -0,0 +1,9 @@
+<?php session_destroy(); ?>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">GRACIAS POR HABER USADO NUESTRO SISTEMA</h1>
+    <h1 class="display-3">PROYECTO DE PROGRAMACIÓN AVANZADA</h1>
+  </div>
+</div>
+
+<meta http-equiv="refresh" content="3; url=index.php"/>
